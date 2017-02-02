4e08d9e1b23eb5de849eda9a83704b3e69dab2db
Maria Aksonova <mashaaks@ukr.net>
2017-02-02 22:50:11 +0200

    SCSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
-/**
- * Created by masha on 01.02.2017.
- */
+$(function (){
+    $('.main-btn').on('click', function () {
+        $('.popup-wrap').fadeIn();
+    });
+    $('.close').on('click', function () {
+        $('.popup-wrap').fadeOut();
+    });
+    $('.popup-wrap:not(.popup)').on('click', function () {
+        $('.popup-wrap').fadeOut();
+    });
+});
